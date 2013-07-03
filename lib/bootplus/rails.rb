f2d7370182433da9f97e375ce9167c332a3613ee
Patorn Utenpattanun <patorn.u@gmail.com>
2013-07-03 20:38:03 +0700

    init commit

---
+++ lib/bootplus/rails.rb
@@ -0,0 +1,7 @@
+require "bootplus/rails/version"
+
+module Bootplus
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/bootplus/rails/version.rb
@@ -0,0 +1,5 @@
+module Bootplus
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
